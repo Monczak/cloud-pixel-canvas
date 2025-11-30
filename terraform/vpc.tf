c6c27fe5e23db94fc2c58370c6eeff2bf901ee7a
Monczak <szymon.jedrzejczak.sj@gmail.com>
2025-12-01 00:17:31 +0100

    feat: Refine VPC config

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,35 +3,61 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
 }
 
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-1"
+  }
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-2"
+  }
 }
 
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.project_name}-private-subnet-1"
+  }
 }
 
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "${var.project_name}-private-subnet-2"
+  }
 }
 
 resource "aws_eip" "nat_1" {
@@ -47,6 +73,10 @@ resource "aws_nat_gateway" "main_1" {
   allocation_id = aws_eip.nat_1.id
 
   depends_on = [aws_internet_gateway.main]
+
+  tags = {
+    Name = "${var.project_name}-nat-1"
+  }
 }
 
 resource "aws_nat_gateway" "main_2" {
@@ -54,6 +84,10 @@ resource "aws_nat_gateway" "main_2" {
   allocation_id = aws_eip.nat_2.id
 
   depends_on = [aws_internet_gateway.main]
+
+  tags = {
+    Name = "${var.project_name}-nat-2"
+  }
 }
 
 resource "aws_route_table" "public" {
@@ -62,6 +96,10 @@ resource "aws_route_table" "public" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-rtb-public"
   }
 }
 
@@ -72,6 +110,10 @@ resource "aws_route_table" "private_1" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.main_1.id
   }
+
+  tags = {
+    Name = "${var.project_name}-rtb-private-1"
+  }
 }
 
 resource "aws_route_table" "private_2" {
@@ -80,6 +122,10 @@ resource "aws_route_table" "private_2" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.main_2.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-rtb-private-2"
   }
 }
 
@@ -101,4 +147,24 @@ resource "aws_route_table_association" "private_1" {
 resource "aws_route_table_association" "private_2" {
   subnet_id      = aws_subnet.private_2.id
   route_table_id = aws_route_table.private_2.id
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id          = aws_vpc.main.id
+  service_name    = "com.amazonaws.${var.aws_region}.s3"
+  route_table_ids = [aws_route_table.private_1.id, aws_route_table.private_2.id]
+
+  tags = {
+    Name = "${var.project_name}-s3-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id          = aws_vpc.main.id
+  service_name    = "com.amazonaws.${var.aws_region}.dynamodb"
+  route_table_ids = [aws_route_table.private_1.id, aws_route_table.private_2.id]
+
+  tags = {
+    Name = "${var.project_name}-dynamodb-endpoint"
+  }
 }
